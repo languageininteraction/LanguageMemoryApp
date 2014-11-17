93be5e5c9dd0fb20c21edf68000b566644d13688
Peter Withers <peter-gthb@bambooradical.com>
2014-11-17 14:26:36 +0100

    Created versions of the splash screen without stretching for each of the various screen sizes required for iOS and Android. closes #102 Also created patch nine files but found they did not get used correctly. closes #103

--- Synaesthesia/setup-cordova.sh
+++ Synaesthesia/setup-cordova.sh
@@ -14,24 +14,40 @@ splashImage="images/splash.png" #"images/splash.gif" "images/icon.jpg"
 echo $splashImage
 #file ./platforms/ios/SynQuiz/Resources/splash/*
 
+echo "making 9 patch splash images"
+convert -background none images/LiI_logo_rgb.jpg -resize 320x320 -matte -bordercolor "rgb(0,158,200)" -border 2 -fill black -draw "line 1,0 1,0" -draw "line 0,1 0,1" -draw "line 0,67 0,67" -draw "line 322,0 322,0" platforms/splash320x320.9.png
+#draw9patch platforms/splash320x320.9.png
+convert -background none images/LiI_logo_rgb.jpg -resize 150x150 -matte -bordercolor "rgb(0,158,200)" -border 2 -fill black -draw "line 1,0 1,0" -draw "line 0,1 0,1" -draw "line 0,32 0,32" -draw "line 152,0 152,0" platforms/splash150x150.9.png
+#draw9patch platforms/splash150x150.9.png
+
 echo "making iOS splash images"
-convert -geometry 640x1136 -quality 100 $splashImage $splashResourcesDir/Default-568h@2x~iphone.png
-convert -geometry 2048x1536 -quality 100 $splashImage $splashResourcesDir/Default-Landscape@2x~ipad.png
-convert -geometry 1024x768 -quality 100 $splashImage $splashResourcesDir/Default-Landscape~ipad.png
-convert -geometry 1536x2048 -quality 100 $splashImage $splashResourcesDir/Default-Portrait@2x~ipad.png
-convert -geometry 768x1024 -quality 100 $splashImage $splashResourcesDir/Default-Portrait~ipad.png
-convert -geometry 640x960 -quality 100 $splashImage $splashResourcesDir/Default@2x~iphone.png
-convert -geometry 320x480 -quality 100 $splashImage $splashResourcesDir/Default~iphone.png
+#convert -resize 640x1136^ -gravity center -extent 640x1136 -quality 100 $splashImage $splashResourcesDir/Default-568h@2x~iphone.png
+convert -resize 1136x1136^ -gravity center -extent 640x1136 -quality 100 $splashImage $splashResourcesDir/Default-568h@2x~iphone.png
+convert -resize 2048x2048^ -gravity center -extent 2048x1536 -quality 100 $splashImage $splashResourcesDir/Default-Landscape@2x~ipad.png
+convert -resize 1024x1024^ -gravity center -extent 1024x768 -quality 100 $splashImage $splashResourcesDir/Default-Landscape~ipad.png
+convert -resize 2048x2048^ -gravity center -extent 1536x2048 -quality 100 $splashImage $splashResourcesDir/Default-Portrait@2x~ipad.png
+convert -resize 1024x1024^ -gravity center -extent 768x1024 -quality 100 $splashImage $splashResourcesDir/Default-Portrait~ipad.png
+convert -resize 960x960^ -gravity center -extent 640x960 -quality 100 $splashImage $splashResourcesDir/Default@2x~iphone.png
+convert -resize 480x480^ -gravity center -extent 320x480 -quality 100 $splashImage $splashResourcesDir/Default~iphone.png
 
 echo "making Android splash images"
-convert -geometry 800x480 -quality 100 $splashImage platforms/android/res/drawable-land-hdpi/screen.png
-convert -geometry 320x200 -quality 100 $splashImage platforms/android/res/drawable-land-ldpi/screen.png
-convert -geometry 480x320 -quality 100 $splashImage platforms/android/res/drawable-land-mdpi/screen.png
-convert -geometry 1280x720 -quality 100 $splashImage platforms/android/res/drawable-land-xhdpi/screen.png
-convert -geometry 480x800 -quality 100 $splashImage platforms/android/res/drawable-port-hdpi/screen.png
-convert -geometry 200x320 -quality 100 $splashImage platforms/android/res/drawable-port-ldpi/screen.png
-convert -geometry 320x480 -quality 100 $splashImage platforms/android/res/drawable-port-mdpi/screen.png
-convert -geometry 720x1280 -quality 100 $splashImage platforms/android/res/drawable-port-xhdpi/screen.png
+#cp platforms/splash320x320.9.png platforms/android/res/drawable-land-hdpi/screen.png
+#cp platforms/splash150x150.9.png platforms/android/res/drawable-land-ldpi/screen.png
+#cp platforms/splash150x150.9.png platforms/android/res/drawable-land-mdpi/screen.png
+#cp platforms/splash320x320.9.png platforms/android/res/drawable-land-xhdpi/screen.png
+#cp platforms/splash320x320.9.png platforms/android/res/drawable-port-hdpi/screen.png
+#cp platforms/splash150x150.9.png platforms/android/res/drawable-port-ldpi/screen.png
+#cp platforms/splash150x150.9.png platforms/android/res/drawable-port-mdpi/screen.png
+#cp platforms/splash320x320.9.png platforms/android/res/drawable-port-xhdpi/screen.png
+
+convert -resize 800x480^ -gravity center -extent 800x480 -quality 100 $splashImage platforms/android/res/drawable-land-hdpi/screen.png
+convert -resize 320x200^ -gravity center -extent 320x200 -quality 100 $splashImage platforms/android/res/drawable-land-ldpi/screen.png
+convert -resize 480x320^ -gravity center -extent 480x320 -quality 100 $splashImage platforms/android/res/drawable-land-mdpi/screen.png
+convert -resize 1280x720^ -gravity center -extent 1280x720 -quality 100 $splashImage platforms/android/res/drawable-land-xhdpi/screen.png
+convert -resize 480x800^ -gravity center -extent 480x800 -quality 100 $splashImage platforms/android/res/drawable-port-hdpi/screen.png
+convert -resize 200x320^ -gravity center -extent 200x320 -quality 100 $splashImage platforms/android/res/drawable-port-ldpi/screen.png
+convert -resize 320x480^ -gravity center -extent 320x480 -quality 100 $splashImage platforms/android/res/drawable-port-mdpi/screen.png
+convert -resize 720x1280^ -gravity center -extent 720x1280 -quality 100 $splashImage platforms/android/res/drawable-port-xhdpi/screen.png
 
 # copy the ant.properties file with the android key store and alias (key.store= key.alias=) information so that the APK can be signed
 cp ~/android-keys/ant.properties platforms/android/
@@ -40,7 +56,7 @@ echo "building"
 #cordova prepare
 #cordova compile
 cordova build -release
-# cordova emulate android 
+#cordova emulate android 
 #cordova emulate ios --target="iPad"
 #cordova emulate ios --target="iPhone"
 
