f70381cc0f0c77aee0252ed2e2c97396b37a6395
Peter Withers <peter-gthb@bambooradical.com>
2014-11-27 11:38:22 +0100

    Added initial code to respond to SVG clicks and extract the element id of the target.

--- gwt-cordova/src/main/java/nl/ru/languageininteraction/language/client/presenter/MapPresenter.java
+++ gwt-cordova/src/main/java/nl/ru/languageininteraction/language/client/presenter/MapPresenter.java
@@ -17,9 +17,13 @@
  */
 package nl.ru.languageininteraction.language.client.presenter;
 
+import com.google.gwt.dom.client.Element;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.safehtml.shared.SafeHtmlBuilder;
 import com.google.gwt.safehtml.shared.SafeHtmlUtils;
 import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import nl.ru.languageininteraction.synaesthesia.client.listener.AppEventListner;
@@ -52,14 +56,26 @@ public class MapPresenter extends AbstractPresenter implements Presenter {
 
     @Override
     public void setContent(AppEventListner appEventListner) {
+        final Label label = new Label();
+        verticalPanel.add(label);
         String[] items = new String[]{"one", "two", "three"};
         SafeHtmlBuilder builder = new SafeHtmlBuilder();
         for (String item : items) {
             builder.appendEscaped(item).appendHtmlConstant("<br/>");
         }
-        builder.append(SafeHtmlUtils.fromTrustedString("<svg><rect height='5px' width='15px' fill='blue'/>"));
-        builder.append(SafeHtmlUtils.fromTrustedString("<svg><rect x='10' y='10'height='88px' width='88px' fill='green'/>"));
-        verticalPanel.add(new HTML(builder.toSafeHtml()));
+        builder.append(SafeHtmlUtils.fromTrustedString("<svg id='thesvg'>"));
+        builder.append(SafeHtmlUtils.fromTrustedString("<rect id='island' height='5px' width='15px' fill='blue'/>"));
+        builder.append(SafeHtmlUtils.fromTrustedString("<rect id='continent' x='10' y='10'height='88px' width='88px' fill='green'/>"));
+        builder.append(SafeHtmlUtils.fromTrustedString("</svg>"));
+        final HTML html = new HTML(builder.toSafeHtml());
+        html.addClickHandler(new ClickHandler() {
+
+            @Override
+            public void onClick(ClickEvent event) {
+                label.setText(Element.as(event.getNativeEvent().getEventTarget()).getId());
+            }
+        });
+        verticalPanel.add(html);
         verticalPanel.add(new HTML(builder.toSafeHtml()));
         simpleView.setContent(verticalPanel);
     }
