8c6ef2db22dda8a9c388a5f488a61f790a50155c
Peter Withers <peter-gthb@bambooradical.com>
2014-12-23 16:18:19 +0100

    Started converting the SVG graphics from InkScape into usable properties files and safehtml. #17 Started the language matching screen. #18

---
+++ gwt-cordova/src/main/java/nl/ru/languageininteraction/language/client/presenter/MatchLanguagePresenter.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2014 Language In Interaction
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package nl.ru.languageininteraction.language.client.presenter;
+
+import com.google.gwt.user.client.ui.RootPanel;
+import nl.ru.languageininteraction.language.client.listener.AppEventListner;
+import nl.ru.languageininteraction.language.client.listener.PresenterEventListner;
+import nl.ru.languageininteraction.language.client.view.MatchLanguageView;
+
+/**
+ * @since Nov 26, 2014 4:12:27 PM (creation date)
+ * @author Peter Withers <p.withers@psych.ru.nl>
+ */
+public class MatchLanguagePresenter extends AbstractPresenter implements Presenter {
+
+    public MatchLanguagePresenter(RootPanel widgetTag) {
+        super(widgetTag, new MatchLanguageView());
+    }
+
+    @Override
+    public void setTitle(PresenterEventListner titleBarListner) {
+        simpleView.addTitle(messages.mapScreenTitle(), titleBarListner);
+    }
+
+    @Override
+    public void setContent(AppEventListner appEventListner) {
+        ((MatchLanguageView) simpleView).setupScreen();
+    }
+}
