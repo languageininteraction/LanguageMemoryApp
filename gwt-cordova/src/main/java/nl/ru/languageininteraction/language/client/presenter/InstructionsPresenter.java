51ca52ca9485e5d6f32f40da37e4a2082b1d04a8
Peter Withers <peter-gthb@bambooradical.com>
2015-01-06 16:29:17 +0100

    Added application states to the app controller and migrated some required classes. #13

--- gwt-cordova/src/main/java/nl/ru/languageininteraction/language/client/presenter/InstructionsPresenter.java
+++ gwt-cordova/src/main/java/nl/ru/languageininteraction/language/client/presenter/InstructionsPresenter.java
@@ -15,7 +15,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
-package nl.ru.languageininteraction.synaesthesia.client.presenter;
+package nl.ru.languageininteraction.language.client.presenter;
 
 import com.google.gwt.user.client.ui.RootPanel;
 import nl.ru.languageininteraction.language.client.listener.AppEventListner;
