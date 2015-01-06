6c94a43d9da66491fd29bafd4ef5b06bf392f78e
Peter Withers <peter-gthb@bambooradical.com>
2015-01-06 11:42:31 +0100

    Added a bash script to retrieve the audio samples for the project, to be run manually once prior to compiling. #20

---
+++ gwt-cordova/src/main/media/getmediafiles.sh
@@ -0,0 +1,7 @@
+# download media files required for mobile app compilation
+# @since Jan 6, 2015 10:38 AM (creation date)
+# @author Peter Withers <p.withers@psych.ru.nl>
+
+curl -L https://www.dropbox.com/sh/2phs18cyrskm24y/AAC4zKXPu7FMkq_9j8B2-lpYa/Sample1a.wav?dl=0 -o "Sample1.wav"
+curl -L https://www.dropbox.com/sh/2phs18cyrskm24y/AACqgUYXbAedPL0dlIijsszea/Sample2a.wav?dl=0 -o "Sample2.wav"
+curl -L https://www.dropbox.com/sh/2phs18cyrskm24y/AADTTLPKWC4T-rS_5RSdQafNa/Sample3a.wav?dl=0 -o "Sample3.wav"
