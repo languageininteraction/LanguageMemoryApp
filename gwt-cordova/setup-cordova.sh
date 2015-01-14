38abc4299a7152eb87cea8698b9bdd2c70e09d88
Peter Withers <peter-gthb@bambooradical.com>
2015-01-14 17:12:14 +0100

    Bumped the build number,

--- gwt-cordova/setup-cordova.sh
+++ gwt-cordova/setup-cordova.sh
@@ -1,6 +1,6 @@
 #mvn install
 cd target
-synquizname=synquiz-1.0.78-testing
+synquizname=synquiz-1.0.80-testing
 rm -rf $synquizname-cordova
 unzip $synquizname-cordova.zip -d $synquizname-cordova
 cd $synquizname-cordova
