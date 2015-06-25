9ebddf927e80eb18d0f99faa47ea6acbcd3301f8
Peter Withers <peter.withers@mpi.nl>
2015-06-25 17:54:36 +0200

    Applied the new translation text. Set the agree text so that it uses the translated texts. Added the border to the Guess Round screen. Updated the tutorial screen and made sure the eyes are on the head. Reverted to ascii characters for text in the SVG.

--- gwt-cordova/src/main/java/nl/ru/languageininteraction/language/client/presenter/ExplainDataSharingScreenPresenter.java
+++ gwt-cordova/src/main/java/nl/ru/languageininteraction/language/client/presenter/ExplainDataSharingScreenPresenter.java
@@ -17,9 +17,11 @@
  */
 package nl.ru.languageininteraction.language.client.presenter;
 
+import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.RootLayoutPanel;
 import nl.ru.languageininteraction.language.client.ExplainDataSharingScreenBuilder;
+import nl.ru.languageininteraction.language.client.Messages;
 import nl.ru.languageininteraction.language.client.exception.AudioException;
 import nl.ru.languageininteraction.language.client.listener.AppEventListner;
 import nl.ru.languageininteraction.language.client.listener.PresenterEventListner;
@@ -36,6 +38,7 @@ import nl.ru.languageininteraction.language.client.view.ExplainDataSharingScreen
 public class ExplainDataSharingScreenPresenter extends AbstractSvgPresenter implements Presenter {
 
     protected final ExplainDataSharingScreenBuilder svgBuilder = new ExplainDataSharingScreenBuilder();
+    private final Messages messages = GWT.create(Messages.class);
 
     public ExplainDataSharingScreenPresenter(RootLayoutPanel widgetTag, final UserResults userResults, AudioPlayer audioPlayer, final AppEventListner appEventListner) throws AudioException {
         super(widgetTag, userResults, audioPlayer, new ExplainDataSharingScreenView(new PresenterEventListner() {
@@ -71,6 +74,19 @@ public class ExplainDataSharingScreenPresenter extends AbstractSvgPresenter impl
 
     @Override
     void configureSvg() {
+        svgBuilder.setLabel(ExplainDataSharingScreenBuilder.SvgTextElements.tspan6492, messages.permissionLine1());
+        svgBuilder.setLabel(ExplainDataSharingScreenBuilder.SvgTextElements.tspan6488, messages.permissionLine2());
+        svgBuilder.setLabel(ExplainDataSharingScreenBuilder.SvgTextElements.tspan6352, messages.permissionLine3());
+        svgBuilder.setLabel(ExplainDataSharingScreenBuilder.SvgTextElements.tspan6360, messages.permissionLine4());
+        svgBuilder.setLabel(ExplainDataSharingScreenBuilder.SvgTextElements.tspan6368, messages.permissionLine5());
+        svgBuilder.setLabel(ExplainDataSharingScreenBuilder.SvgTextElements.tspan6376, messages.permissionLine6());
+        svgBuilder.setLabel(ExplainDataSharingScreenBuilder.SvgTextElements.tspan6384, messages.permissionLine7());
+        svgBuilder.setLabel(ExplainDataSharingScreenBuilder.SvgTextElements.tspan6392, messages.permissionLine8());
+        svgBuilder.setLabel(ExplainDataSharingScreenBuilder.SvgTextElements.tspan6400, messages.permissionLine9());
+        svgBuilder.setLabel(ExplainDataSharingScreenBuilder.SvgTextElements.tspan6408, messages.permissionLine10());
+        svgBuilder.setLabel(ExplainDataSharingScreenBuilder.SvgTextElements.tspan6416, messages.permissionLine11());
+        svgBuilder.setLabel(ExplainDataSharingScreenBuilder.SvgTextElements.tspan5602, messages.yesText());
+        svgBuilder.setLabel(ExplainDataSharingScreenBuilder.SvgTextElements.tspan6386, messages.noText());
     }
 
     @Override
